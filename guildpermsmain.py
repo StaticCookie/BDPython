1adf6b926ba612385f16e1e9b1c24b5e8f7d9908
Brent <248846061+StaticCookie@users.noreply.github.com>
2026-03-28 11:10:24 +1100

    Add guild permission functions for user checks

---
+++ guildpermsmain.py
@@ -0,0 +1,24 @@
+can_create_guild = 0b1000
+can_review_guild = 0b0100
+can_delete_guild = 0b0010
+can_edit_guild = 0b0001
+
+
+def get_create_bits(user_permissions):
+    permission_check = can_create_guild & user_permissions
+    return permission_check
+
+
+def get_review_bits(user_permissions):
+    permission_check = can_review_guild & user_permissions
+    return permission_check
+
+
+def get_delete_bits(user_permissions):
+    permission_check = can_delete_guild & user_permissions
+    return permission_check
+
+
+def get_edit_bits(user_permissions):
+    permission_check = can_edit_guild & user_permissions
+    return permission_check
