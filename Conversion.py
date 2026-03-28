0c8082af6627e1f430ca6e0506bb1deb199afcf2
Brent <248846061+StaticCookie@users.noreply.github.com>
2026-03-28 11:54:02 +1100

    Add binary_string_to_int function for conversion

---
+++ Conversion.py
@@ -0,0 +1,7 @@
+
+## Convert Binary to Numbers 
+def binary_string_to_int(num_servers, num_players, num_admins):
+    num_servers = int(num_servers, 2)
+    num_players = int(num_players, 2)
+    num_admins = int(num_admins, 2)
+    return num_servers, num_players, num_admins
