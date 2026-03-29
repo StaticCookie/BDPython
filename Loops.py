6edbb98451be05620e494081ff2662be27a9bae5
Brent <248846061+StaticCookie@users.noreply.github.com>
2026-03-29 16:33:50 +1100

    Update Loops.py

--- Loops.py
+++ Loops.py
@@ -19,3 +19,12 @@ def sum_of_numbers(start, end):
     for i in range(start, end):
         total += i
     return total
+
+## Loop that counts odd numbers
+def sum_of_odd_numbers(end):
+    total = 0
+    for i in range(1, end, 2):
+        total += i
+    return total
+
+## While loop
