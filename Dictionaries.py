0d6909da87862438b2ea66cff2a07a8485fa6db6
Brent <248846061+StaticCookie@users.noreply.github.com>
2026-04-06 20:42:09 +1000

    Update Dictionaries.py

--- Dictionaries.py
+++ Dictionaries.py
@@ -14,3 +14,15 @@ def get_character_record(name, server, level, rank):
 
     return account_information 
 
+#----------------------------------------------------------------------------#
+# dicationary process that counts duplicate values and returns the count
+#----------------------------------------------------------------------------#
+def count_enemies(enemy_names):
+    enemies_dict = {}
+    for enemy_name in enemy_names:
+        
+        if enemy_name not in enemies_dict:
+            enemies_dict[enemy_name] = 1
+        else: 
+            enemies_dict[enemy_name] += 1
+    return enemies_dict
