4333caf278b92f2e513f369e556b162ef8b0b03f
Brent <248846061+StaticCookie@users.noreply.github.com>
2026-04-09 22:32:43 +1000

    Create ErrorHandle.py

---
+++ ErrorHandle.py
@@ -0,0 +1,24 @@
+#----------------------------------------------------------------------------#
+# Calls function and checks for varios errors 
+  # Index Error returns: Index is too high
+  # custom error for negative integers
+  # Catch all error
+#----------------------------------------------------------------------------#
+def process_player_record(player_id):
+    try:
+        get_player_record(player_id)
+        return get_player_record(player_id)
+    except IndexError: # Index Error returns: Index is too high
+        return "index is too high"
+    except Exception as e: # Catch all error
+            return str(e) 
+
+def get_player_record(player_id):
+    if player_id < 0:
+        raise Exception("negative ids not allowed") # custom error for negative integers
+    players = [
+        {"name": "Slayer", "level": 128},
+        {"name": "Dorgoth", "level": 300},
+        {"name": "Saruman", "level": 4000},
+    ]
+    return players[player_id]
